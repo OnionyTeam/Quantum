fe9e7f51ba9b142faba5894dc33a7a2c43abdb87
flyingpig233-cpu <1686079252@qq.com>
2022-04-16 21:36:28 +0800

    Created global_config.h, it saved some configs

---
+++ src/global_config.h
@@ -0,0 +1,22 @@
+/**
+ *  Descirption:
+ *      This header file contains some global configuration.
+ */
+
+
+#ifndef _QUANTUM_GLOBAL_CONFIG_H__
+#define _QUANTUM_GLOBAL_CONFIG_H__
+
+#include <string>
+
+// program description
+namespace config {
+
+    static std::string program_description = "Quantum is a console-run editor and it has special shortcuts to help "
+        "people code quickly. But it's still in the preparatory stage";
+
+} // namespace config
+
+
+
+#endif //_QUANTUM_GLOBAL_CONFIG_H__
